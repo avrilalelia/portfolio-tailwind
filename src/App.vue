19c776af6662158fcc436cdbe1efb939aa846b43
avrilalelia <avrilalelia01@gmail.com>
2022-03-24 11:19:52 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Zen+Antique&display=swap"
+    rel="stylesheet"
+  />
+  <HeaderPart />
+  <HeroPart />
+  <MyWorkPart />
+  <ClientsPart />
+  <HireMePart />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+function updateList() {
+	const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
+		return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+	});
+
+	document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+	
+	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+}
+
+updateList();
+window.addEventListener('scroll', () => {
+    updateList();
+})
+
+import HeaderPart from "./components/Header.vue";
+import HeroPart from "./components/Hero.vue";
+import MyWorkPart from "./components/MyWork.vue";
+import ClientsPart from "./components/Clients.vue";
+import HireMePart from "./components/HireMe.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderPart, HeroPart, MyWorkPart, ClientsPart, HireMePart
+  },
+  mounted() {
+    document.body.classList.add("bg-body", "text-white", "font-poppins", "pb-12");
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+ <header class="py-6">
+  <div class="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+   <div class="text-lg font-bold">Avril.alelia</div>
+   <div class="hidden md:flex space-x-12 items-center">
+    <a href="#" class="text-selected-text">Home</a>
+    <a href="#work">My Work</a>
+    <a href="#clients">Clients</a>
+    <a href="#hire"><button class="px-6 py-2 bg-theme font-bold">Hire Me</button></a>
+   </div>
+   <div class="md:hidden">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+    </div>
+ </div>
+ </header>
+</template>
+
+<script>
+export default {
+ name: 'HeaderPart',
+ setup(){
+ }
+}
+</script>
